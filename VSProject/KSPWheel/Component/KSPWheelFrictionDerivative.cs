fbab0b81d0883720688bd198d169faec58a76844
shadowmage45 <shadowmage45@hotmail.com>
2016-12-29 15:23:27 -0700

    prep on figuring out RK4 integration; need to figure out how to fake rigidbody updates / updated status for derivatives

---
+++ VSProject/KSPWheel/Component/KSPWheelFrictionDerivative.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KSPWheel
+{
+    public class KSPWheelFrictionDerivative
+    {
+        //Input values....
+        public float sLat;//lateral slip
+        public float sLong;//long slip
+        public float wWheel;//wheel angular velocity
+        public Vector3 wheelLocalVelocity;
+
+    }
+}
